351d1d20de6ee43c9b05e9b0a8eee9d5f56b209e
Nihadname <nihadcoding@gmail.com>
2023-12-02 16:39:33 +0400

    all changes are done

--- Struct-Enum-Upcasting-Downcasting-Boxing-Unboxing.Domain/Models/Employee.cs
+++ Struct-Enum-Upcasting-Downcasting-Boxing-Unboxing.Domain/Models/Employee.cs
@@ -29,7 +29,7 @@ namespace Implicit_Explicit_OperatorOverloading_Nullable.Models
             Employee employee1 = new("Nihad1","Ibadzade1",1280,new DateTime(2008,2,12));
             Employee employee2 = new("Nihad2", "Ibadzade2", 480, new DateTime(2002, 2, 12));
             Employee employee3 = new("Nihad3", "Ibadzade3", 9870, new DateTime(2003, 2, 12));
-            Employee employee4 = new("Nihad4", "Ibadzade4", 990, new DateTime(2004, 2, 12));
+            Employee employee4 = new("Nihad4", "Ibadzade4", 99000, new DateTime(2004, 2, 12));
             Employee employee5 = new("Nihad5", "Ibadzade5", 680, new DateTime(2006, 2, 12));
             Employee employee6 = new("Nihad6", "Ibadzade6", 3480, new DateTime(2004, 2, 12));
             Employee[] employees =  { employee1, employee2, employee3, employee4, employee5,employee6 };
